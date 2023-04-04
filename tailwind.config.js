0eb0c354a50b87306c3763a3c6e00b8a49a7724b
Omar Coronado <omar.goco@gmail.com>
2023-04-04 14:54:42 +0100

    agrega estilos de gobierno federal

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                success: {
+                    DEFAULT: '#13322B',
+                    50: '#46B79D',
+                    100: '#40A891',
+                    200: '#358B77',
+                    300: '#296D5E',
+                    400: '#1E5044',
+                    500: '#13322B',
+                    600: '#040908',
+                    700: '#000000',
+                    800: '#000000',
+                    900: '#000000',
+                    950: '#000000'
+                },
+                primary: {
+                    DEFAULT: '#9D2449',
+                    50: '#E692AC',
+                    100: '#E2829F',
+                    200: '#DB6186',
+                    300: '#D3406D',
+                    400: '#BE2C58',
+                    500: '#9D2449',
+                    600: '#6F1A34',
+                    700: '#420F1F',
+                    800: '#140509',
+                    900: '#000000',
+                    950: '#000000'
+                },
+                warning: {
+                    DEFAULT: '#B38E5D',
+                    50: '#EEE5DA',
+                    100: '#E7DBCC',
+                    200: '#DAC8B0',
+                    300: '#CDB595',
+                    400: '#C0A179',
+                    500: '#B38E5D',
+                    600: '#937145',
+                    700: '#6D5433',
+                    800: '#473621',
+                    900: '#20190F',
+                    950: '#0D0A06'
+                },
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
