cc44b4b0d9e6d257240d25a2d106b758e80b3d13
Omar Coronado <omar.goco@gmail.com>
2023-05-19 09:34:26 +0100

    agrega relacion de catalogo a catalogo

--- app/Models/Catalogo.php
+++ app/Models/Catalogo.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Catalogo extends Model
 {
     use HasFactory;
+
+    public function cat() {
+        return $this->belongsTo(Catalogo::class);
+    }
 }
