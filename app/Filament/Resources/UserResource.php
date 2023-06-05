ce71d00118d7d30c75876e3cd32aaafbf455a84e
Omar Coronado <omar.goco@gmail.com>
2023-06-05 23:20:58 +0100

    agrega resource para usuarios

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\Catalogo;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Spatie\Permission\Models\Role;
+
+class UserResource extends Resource {
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    protected static ?string $navigationGroup = 'Usuarios y Permisos';
+
+    protected static ?string $label = 'Usuario';
+
+    protected static ?string $pluralLabel = 'Usuarios';
+
+    public static function form(Form $form): Form {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('Nombre')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('last_name')
+                    ->label('Apellido')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('ramo_id')
+                    ->label('Ramo')
+                    ->options(
+                        Catalogo::query()
+                            ->select(['id_ramo', 'desc_ramo'])
+                            ->groupBy('id_ramo', 'desc_ramo')
+                            ->get()
+                            ->mapWithKeys(function ($ramo) {
+                                return [$ramo->id_ramo => "{$ramo->id_ramo} - {$ramo->desc_ramo}"];
+                            })
+                    )
+                    ->required(),
+                Forms\Components\Select::make('roles')
+                ->label('Rol')
+                    ->multiple()
+                    ->relationship('roles','name')
+                ->options(
+                    Role::query()
+                    ->get()
+                    ->pluck('name', 'id')
+                )
+            ]);
+    }
+
+    public static function table(Table $table): Table {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nombre')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('last_name')
+                    ->label('Apellido')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('ramo.desc_ramo')
+                    ->label('Ramo')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Actualizado el')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -19,8 +21,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'last_name',
         'email',
         'password',
+        'ramo_id'
     ];
 
     /**
@@ -41,4 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function ramo(): BelongsTo {
+        return $this->belongsTo(Catalogo::class, 'ramo_id', 'id_ramo')->groupBy('id_ramo', 'id');
+    }
 }
