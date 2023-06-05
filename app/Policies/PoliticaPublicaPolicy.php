e2a9e152f6ee6aa97076d99c437a089027227a8b
Omar Coronado <omar.goco@gmail.com>
2023-06-05 23:20:09 +0100

    agrega filament shield

---
+++ app/Policies/PoliticaPublicaPolicy.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\PoliticaPublica;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PoliticaPublicaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->can('view_any_politica::publica');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, PoliticaPublica $politicaPublica)
+    {
+        return $user->can('view_politica::publica');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->can('create_politica::publica');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, PoliticaPublica $politicaPublica)
+    {
+        return $user->can('update_politica::publica');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, PoliticaPublica $politicaPublica)
+    {
+        return $user->can('delete_politica::publica');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function deleteAny(User $user)
+    {
+        return $user->can('delete_any_politica::publica');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, PoliticaPublica $politicaPublica)
+    {
+        return $user->can('{{ ForceDelete }}');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDeleteAny(User $user)
+    {
+        return $user->can('{{ ForceDeleteAny }}');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, PoliticaPublica $politicaPublica)
+    {
+        return $user->can('{{ Restore }}');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restoreAny(User $user)
+    {
+        return $user->can('{{ RestoreAny }}');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function replicate(User $user, PoliticaPublica $politicaPublica)
+    {
+        return $user->can('{{ Replicate }}');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function reorder(User $user)
+    {
+        return $user->can('{{ Reorder }}');
+    }
+
+}
