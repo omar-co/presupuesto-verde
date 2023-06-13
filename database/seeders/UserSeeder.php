22f3d2845fa73ecb93f35bdc4da65e263749da4e
Omar Coronado <omar.goco@gmail.com>
2023-06-13 20:04:41 +0100

    agrega seeder de usuario

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,15 +6,19 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PoliticaPublicaSeeder extends Seeder {
+class UserSeeder extends Seeder
+{
     /**
      * Run the database seeds.
      */
-    public function run(): void {
-        DB::table('politicas_publicas')->insert([
+    public function run(): void
+    {
+        DB::table('users')->insert([
             [
-                'name' => 'Política Nacional de Medio Ambiente',
-                'user_id' => 1,
+                'name' => 'Omar',
+                'last_name' => 'Coronado',
+                'email' => 'omar.goco@gmail.com',
+                'password' => '$2y$10$83gpNmvkG1L0/N4UVigCCu4P5.3n1Udc.aCrIrrQOeTwSwRzgttwa',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
