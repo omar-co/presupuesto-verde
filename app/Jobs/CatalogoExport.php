740271e08637d7f8c2cb8d34bae04b1aa76af42c
Omar Coronado <omar.goco@gmail.com>
2023-10-01 14:56:41 +0100

    muestra realpath

--- app/Jobs/CatalogoExport.php
+++ app/Jobs/CatalogoExport.php
@@ -11,6 +11,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
 use Rap2hpoutre\FastExcel\FastExcel;
 
 class CatalogoExport implements ShouldQueue
@@ -39,7 +40,17 @@ class CatalogoExport implements ShouldQueue
         $date = Carbon::now()->format('d-m-Y-H-i-s');
         $basePath = 'storage/app/';
         $path = "exports/catalogo/programas_presupuestales_{$date}.csv";
+
+        if(!File::isDirectory('storage/app/exports/catalogo')){
+
+            File::makeDirectory('storage/app/exports/catalogo', 0777, true, true);
+
+        }
+
         info(realpath('storage/app/exports/catalogo'));
+        $this->export->path = realpath('storage/app/exports/catalogo');
+        $this->export->save();
+
         (new FastExcel($this->catalogoGenerator()))->export($basePath . $path);
 
         $this->export->path = $path;
